57d7911893619575e6868d751a34cfde19e05c19
Diego-monfort-landot <diegomonfort1989@gmail.com>
2024-01-12 20:25:39 +0100

    added new styles and a login btn

--- landing-diego/src/app/user/user.component.ts
+++ landing-diego/src/app/user/user.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { GameComponent } from '../game/game.component';
 import { CommonModule } from '@angular/common';
+import { animate } from '@angular/animations';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [CommonModule, GameComponent],
+  imports: [CommonModule, GameComponent ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
   username = 'DiegoDev';
-  isLoggedIn = true;
+  isLoggedIn = false;
+  greet() {
+    alert('¡¡Not Open this Photo!!')
+  }
 
 }
